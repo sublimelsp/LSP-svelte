5b50e1eeb268d1961d1f9723d1fb8a1c209ba7df
Rafał Chłodnicki <rchl2k@gmail.com>
2021-02-24 00:06:34 +0100

    Fix: Use ${node_bin} for the node binary
    With the latest version of lsp_utils a change was introduced [1] that allows using a locally
    managed node runtime instead of the system one. For that to work, the "node" command
    needs to use a variable.
    
    [1] https://github.com/sublimelsp/lsp_utils/commit/403345a0c5c15e84802c712044c630a9fb236b9d

--- plugin.py
+++ plugin.py
@@ -1,6 +1,5 @@
-import os
-import sublime
 from lsp_utils import NpmClientHandler
+import os
 
 
 def plugin_loaded():
@@ -15,7 +14,3 @@ class LspSveltePlugin(NpmClientHandler):
     package_name = __package__
     server_directory = "server"
     server_binary_path = os.path.join(server_directory, "node_modules", "svelte-language-server", "bin", "server.js")
-
-    @classmethod
-    def install_in_cache(cls) -> bool:
-        return False

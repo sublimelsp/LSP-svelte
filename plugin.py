f57ad9b9e162cfc61d61fe69796b2459796b4fd7
Rafał Chłodnicki <rchl2k@gmail.com>
2026-05-29 21:34:30 +0200

    refactor: migrate to LspPlugin (#173)

--- plugin.py
+++ plugin.py
@@ -1,18 +1,31 @@
 from __future__ import annotations
 
-from lsp_utils import NpmClientHandler
-import os
+from LSP.plugin import LspPlugin
+from LSP.plugin import OnPreStartContext
+from lsp_utils import NodeManager
+from pathlib import Path
+from sublime_lib import ResourcePath
+from typing_extensions import override
 
 
 def plugin_loaded():
-    LspSveltePlugin.setup()
+    LspSveltePlugin.register()
 
 
 def plugin_unloaded():
-    LspSveltePlugin.cleanup()
+    LspSveltePlugin.unregister()
 
 
-class LspSveltePlugin(NpmClientHandler):
-    package_name = str(__package__)
-    server_directory = "server"
-    server_binary_path = os.path.join(server_directory, "node_modules", "svelte-language-server", "bin", "server.js")
+class LspSveltePlugin(LspPlugin):
+
+    @classmethod
+    @override
+    def on_pre_start_async(cls, context: OnPreStartContext) -> None:
+        package_name = cls.plugin_storage_path.name
+        NodeManager.on_pre_start_async(
+            context,
+            cls.plugin_storage_path,
+            ResourcePath("Packages", package_name, "server"),
+            Path("node_modules", "svelte-language-server", "bin", "server.js"),
+            node_version_requirement=">=14",
+        )
